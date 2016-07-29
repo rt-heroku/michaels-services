b5a90117a99d00db00f9ee356f4870ea9727a23d
rt-heroku <rtorres@heroku.com>
2016-07-29 14:27:38 -0400

    Commiting another change on Fri Jul 29 14:27:38 EDT 2016

--- src/main/java/com/heroku/demo/config/Demandware.java
+++ src/main/java/com/heroku/demo/config/Demandware.java
@@ -5,7 +5,8 @@ public class Demandware {
 	private String url;
 	private String clientId;
 	private String apiVersion;
-	
+	private String username;
+	private String secret;
 	
 	
 	public Demandware(String url, String clientId, String apiVersion) {
@@ -20,7 +21,8 @@ public class Demandware {
 		setUrl(isNull(System.getenv("DW_URL"), "http://mjacob01-inside-na01-dw.demandware.net"));
 		setClientId(isNull(System.getenv("DW_CLIENTID"), "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 		setApiVersion(isNull(System.getenv("DW_API_VERSION"), "v16_4"));
-		
+		setUsername(isNull(System.getenv("DW_USERNAME"),""));
+		setSecret(isNull(System.getenv("DW_SECRET"),""));
 	}
 	
 	private String isNull(String val, String d){
@@ -48,6 +50,25 @@ public class Demandware {
 	public void setApiVersion(String apiVersion) {
 		this.apiVersion = apiVersion;
 	}
-	
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getSecret() {
+		return secret;
+	}
+
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+
 	
 }
